d958eef20941f5b704224a365544f355336896b4
dalay <ussria@gmail.com>
2026-07-12 15:36:27 +0300

    upd colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,11 +1,9 @@
--- vim.pack.add({
--- 	"https://github.com/folke/tokyonight.nvim"
--- })
--- vim.pack.add({
---     "https://github.com/wtfox/jellybeans.nvim"
--- })
-vim.pack.add { { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } }
+vim.pack.add({
+    "https://github.com/folke/tokyonight.nvim",
+    "https://github.com/wtfox/jellybeans.nvim",
+    { src = "https://github.com/catppuccin/nvim", name = "catppuccin" }
+})
 
-vim.cmd[[colorscheme catppuccin-nvim]]
+-- vim.cmd[[colorscheme catppuccin-nvim]]
 -- vim.cmd[[colorscheme jellybeans]]
--- vim.cmd[[colorscheme tokyonight-night]]
+vim.cmd[[colorscheme tokyonight-night]]
