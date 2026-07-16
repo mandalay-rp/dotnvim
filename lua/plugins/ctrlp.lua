964aefa32a515d4bbe64f56287b3a06e1521809d
dalay <ussria@gmail.com>
2026-07-16 14:17:33 +0300

    disable telescope, add ctrlP custom settings

--- lua/plugins/ctrlp.lua
+++ lua/plugins/ctrlp.lua
@@ -1,1 +1,12 @@
-vim.pack.add{"https://github.com/ctrlpvim/ctrlp.vim"}
+vim.pack.add({ "https://github.com/ctrlpvim/ctrlp.vim" })
+
+-- По умолчанию, CtrlP ищет файлы по их полному пути, что мне показалось странным, поскольку на запрос "repo", имея следующую структуру файлов:
+-- project/src/Repository/Repository.php
+-- project/src/Repository/Foo.php
+-- CtrlP может придать больший вес файлу Foo.php. К счастью, CtrlP, как и многие другие плагины для vim'a, имеет гибкую настройку. Для решения проблемы нужно прописать
+--
+vim.g.ctrlp_by_filename = 1
+-- откуда начинать поиск. w - ближайшая директория, которая содержит признаки наличия CVS (.git, .svn). r - текущая директория (pwd).
+vim.g.ctrlp_working_path_mode = "wr"
+vim.g.ctrlp_map = "<c-p>" -- открываем нажатием Ctrl+P
+vim.g.ctrlp_cmd = "CtrlPBuffer" -- показывать список буферов по-умолчанию
